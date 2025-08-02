acb348de68f9217b0a1baa9df745b7d710980551
Jking17-AI <biodun.onifade@gmail.com>
2025-08-02 11:38:14 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+ let slideIndex = 1;
+showSlides(slideIndex)
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+
+prevButton.addEventListener("click", () => plusSlides(-1)); 
+nextButton.addEventListener("click", () => plusSlides(1));
+
+
+showSlides(slideIndex);
+
+
+function currentSlide(n){
+      showSlides(slideIndex=n)
+}
